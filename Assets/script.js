093f151d830f8fb7c56ca17411d1c752e36be5a4
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-02-22 02:56:27 -0600

    minor changes to api for forecast instead of current weather

--- Assets/script.js
+++ Assets/script.js
@@ -11,6 +11,7 @@
         var city = $("#searchCity").val().trim();
          if (city != '') {  
             weatherCity(city);
+            weathercards();
         };
 }); 
 
@@ -18,14 +19,16 @@
 function weatherCity (city) {
     console.log(city);
     var key = '9c797ab1cb0980f8f94ba90e9619d5fd';
-    var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key;
+    var url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + key;
     console.log(url);
     $.ajax({
         url: url,
         method: "GET"
     }).then(function (response) {
         return response.json();
-    })
+        var name = response['name'];
+        console.log(name);
+    });
 };
 
 function weathercards () {
